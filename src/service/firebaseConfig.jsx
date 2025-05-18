d340c2e171a83412df8cce9a8c8fada37d5eeb23
royananya605 <109812052+royananya605@users.noreply.github.com>
2025-05-18 10:55:12 +0530

    Update firebaseConfig.jsx

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAKtvryiwrpcXgl7VaruTFWfSg-DJOUz6E",
-  authDomain: "potherpanchaliai.firebaseapp.com",
-  projectId: "potherpanchaliai",
-  storageBucket: "potherpanchaliai.firebasestorage.app",
-  messagingSenderId: "368317605636",
-  appId: "1:368317605636:web:7d4b044d450a6a3fe89251",
-  measurementId: "G-JP74EVRLWS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

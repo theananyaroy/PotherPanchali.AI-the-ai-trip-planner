6d990a74c79dd6d688eaf574fc0544feb2a32b13
Ananya Roy <109812052+theananyaroy@users.noreply.github.com>
2025-07-21 19:37:55 +0530

    Update AiModel.jsx

--- src/service/AiModel.jsx
+++ src/service/AiModel.jsx
@@ -4,7 +4,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "AIzaSyCHtkYXFviMBwK7YP4RS603A8Q4ml-K66w";
+  const apiKey = "your evn key";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({

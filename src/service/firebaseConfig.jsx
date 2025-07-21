5b90696121b84fe247a068b23f0a3e017161b6ce
Ananya Roy <109812052+theananyaroy@users.noreply.github.com>
2025-07-21 19:40:34 +0530

    Update firebaseConfig.jsx

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAKtvryiwrpcXgl7VaruTFWfSg-DJOUz6E",
+  apiKey: "your evn key",
   authDomain: "potherpanchaliai.firebaseapp.com",
   projectId: "potherpanchaliai",
   storageBucket: "potherpanchaliai.firebasestorage.app",
